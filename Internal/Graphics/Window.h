35ccc65e8bdc77d4ad10c9f1b9738f5fbe6d8ca6
FatalError <ripcode0@gmail.com>
2024-04-17 16:23:07 +0900

    updated

--- Internal/Graphics/Window.h
+++ Internal/Graphics/Window.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
 #include "Common/Config.h"
 #include <assert.h>
+#include "Timer.h"
 
 struct WindowState
 {
@@ -31,10 +29,11 @@ public:
 
     //PFN
     PFNWINRESIZE pfnOnResize{};
-        
+
     int width;
     int height;
     HWND mHwnd;
+    Timer mTimer;
     LRESULT LocalWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
 private:
     WindowState mState{};
